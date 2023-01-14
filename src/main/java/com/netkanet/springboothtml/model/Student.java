3265a6096ae225a6eb612de70e0c181678b3bd19
Admy Husky <admyhusky@mac.local>
2023-01-15 06:25:46 +0700

    init project

---
+++ src/main/java/com/netkanet/springboothtml/model/Student.java
@@ -0,0 +1,11 @@
+package com.netkanet.springboothtml.model;
+
+public class Student {
+    private String id;
+    private String name;
+    private String surname;
+    private String address1;
+    private String district;
+    private String province;
+    private String postalcode;
+}
---
+++ src/test/java/com/netkanet/springboothtml/SpringbootHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netkanet.springboothtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
